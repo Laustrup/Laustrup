d694512e180d747245f7e211faae0de4e1731d58
Laustrup <laust.bonnesen@mail.com>
2022-06-13 15:37:36 +0200

    Initial github pages files

---
+++ ressources/static/js/website_render.js
@@ -0,0 +1,5 @@
+const urlDomain = "laustrup.github.io/Laustrup/";
+
+function renderBandwich() { changeLocation(urlDomain + "Bandwich"); }
+
+function changeLocation(urlLocation) { window.location.href = urlLocation; }
